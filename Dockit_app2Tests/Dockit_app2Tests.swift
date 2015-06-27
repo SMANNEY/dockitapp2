a430f8d6b869fc10f1bb55f251809ed0c81f8d14
Shelby Manney <info@zone9.com>
2015-06-26 17:33:58 -0700

    Initial Commit

---
+++ Dockit_app2Tests/Dockit_app2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Dockit_app2Tests.swift
+//  Dockit_app2Tests
+//
+//  Created by Shelby Manney on 6/26/15.
+//  Copyright (c) 2015 DockIt. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Dockit_app2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
